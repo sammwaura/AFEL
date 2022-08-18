69d5ae8f9f5bceeda73737070993048f7698e034
sammwaura <meshsami27@gmail.com>
2022-08-18 12:10:30 +0300

    add new images

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -4,9 +4,18 @@ let navbar = document.querySelector('.header .navbar');
 menu.onclick = () =>{
     menu.classList.toggle('fa-times');
     navbar.classList.toggle('active');
-}
+};
 
 window.onscroll = () =>{
     menu.classList.remove('fa-times');
     navbar.classList.remove('active');
-}
+};
+
+
+var swiper = new Swiper(".home-slider", {
+    loop:true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
